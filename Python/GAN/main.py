e0aff66a59b0a4299f8eb31f63961364dbe25c42
llNovall <25434267+llNovall@users.noreply.github.com>
2022-08-01 17:03:39 +0200

    Updated gitignore to ignore ckpt files

--- Python/GAN/main.py
+++ Python/GAN/main.py
@@ -1,36 +1,60 @@
-from GANDataLoader import GANDataModule
+from GANDataHandler import GANDataModule
 from GANModel import GAN
 from pytorch_lightning.callbacks import TQDMProgressBar, ModelCheckpoint
 from pytorch_lightning import Trainer
 import torch
 from pytorch_lightning.loggers import wandb
+from GANConfig import GANConfig
+import os
 
 if __name__ == '__main__':
 
-    data_location = 'Data/'
+    training_params = GANConfig.training_params
+    model_params = GANConfig.model_params
+    data_module_params = GANConfig.data_module_params
+
+    data_module_params.ds_source_path = 'Data/processed_anime_images'
+    data_module_params.ds_enable_preprocess_images = False
 
     checkpoint_callback = ModelCheckpoint(dirpath='Checkpoints', filename='{epoch}-{train_steps}',
                                           auto_insert_metric_name=True, every_n_train_steps=500,
                                           save_last=True)
 
-    model = GAN(d_hidden_size=64, g_latent_dim=100, g_hidden_size=64,
-                learning_rate=0.002, beta=(0.5, 0.999),
-                weight_decay=0.0, sample_interval=1000)
+    model = GAN(model_params)
 
-    model.initialize_weights()
+    if os.path.exists('Checkpoints/last.ckpt') and training_params.load_from_last_checkpoint:
+        model = GAN.load_from_checkpoint('Checkpoints/last.ckpt')
+    else:
+        model.initialize_weights()
 
-    dm = GANDataModule(path=data_location, image_size=128, train_batch_size=64)
+    dm = GANDataModule(data_module_params)
 
-    wander_logger = wandb.WandbLogger(project='GANTest', log_model='all')
-    wander_logger.watch(model)
+    if training_params.enable_logger:
 
-    trainer = Trainer(
-        logger=wander_logger,
-        accelerator="gpu",
-        devices=1 if torch.cuda.is_available() else None,  # limiting got iPython runs
-        max_epochs=100,
-        callbacks=[TQDMProgressBar(refresh_rate=20), checkpoint_callback],
-        enable_checkpointing=True
-    )
+        wander_logger = None
 
-    trainer.fit(model, dm, ckpt_path="Checkpoints/last.ckpt")
+        wander_logger = wandb.WandbLogger(project='GANTest', log_model='all')
+        wander_logger.watch(model)
+
+        trainer = Trainer(
+            logger=wander_logger,
+            accelerator="gpu",
+            devices=1 if torch.cuda.is_available() else None,
+            max_epochs=20,
+            callbacks=[TQDMProgressBar(refresh_rate=10), checkpoint_callback],
+            enable_checkpointing=True
+        )
+    else:
+        trainer = Trainer(
+            accelerator="gpu",
+            devices=1 if torch.cuda.is_available() else None,
+            max_epochs=20,
+            callbacks=[TQDMProgressBar(refresh_rate=10), checkpoint_callback],
+            enable_checkpointing=True,
+
+        )
+
+    if os.path.exists('Checkpoints/last.ckpt') and training_params.load_from_last_checkpoint:
+        trainer.fit(model, dm, ckpt_path='Checkpoints/last.ckpt')
+    else:
+        trainer.fit(model, dm)
